5bfec9e3a303a27d50f52f8092dd539f603df484
Soel <shachar.soel@sap.com>
2018-11-05 00:53:45 +0200

    WIP on chapter 7.

--- scripts/spec_to_chev.js
+++ scripts/spec_to_chev.js
@@ -6,15 +6,85 @@ function createToken(config) {
   allTokens.push(newToken);
 }
 
-const Whitespace = createToken({ name: "Whitespace", pattern: /[ \t]+/ });
-const Alternative = createToken({ name: "Alternative", pattern: /\n|\r\n|\r/ });
+// The negative lookahead is needed to distinguish between an alternative
+// and a regular newline (between rule)
+const Alternative = createToken({
+  name: "Alternative",
+  pattern: /(\n|\r\n|\r)(?![A-z]\w*:)/
+});
+const Whitespace = createToken({ name: "Whitespace", pattern: /[\s]+/ });
 // Must appear before <NonTerminal> due to common prefix
 const RuleName = createToken({ name: "RuleName", pattern: /[A-z]\w*:/ });
-const NonTerminal = createToken({ name: "NonTerminal", pattern: /[A-z]\w*/ });
-const Terminal = createToken({ name: "Terminal", pattern: /[a-z]\w*/ });
+const NonTerminalID = createToken({
+  name: "NonTerminalID",
+  pattern: /[A-z]\w*/
+});
+const TerminalID = createToken({ name: "TerminalID", pattern: /[a-z]\w*/ });
 const LCurly = createToken({ name: "LCurly", pattern: /{/ });
 const RCurly = createToken({ name: "RCurly", pattern: /}/ });
 const LSquare = createToken({ name: "LSquare", pattern: /\[/ });
 const RSquare = createToken({ name: "RSquare", pattern: /]/ });
 
 const SpecLexer = new Lexer(allTokens);
+
+class JavaSpecParser extends Parser {
+  constructor() {
+    super(allTokens);
+
+    const $ = this;
+
+    $.RULE("topRule", () => {
+      $.CONSUME(RuleName);
+      $.AT_LEAST_ONE(() => {
+        $.CONSUME(Alternative);
+        $.SUBRULE($.alternation);
+      });
+    });
+
+    $.RULE("alternation", () => {
+      $.AT_LEAST_ONE(() => {
+        $.SUBRULE($.production);
+      });
+    });
+
+    $.RULE("production", () => {
+      $.OR([
+        { ALT: () => $.CONSUME(TerminalID) },
+        { ALT: () => $.CONSUME(NonTerminalID) },
+        { ALT: () => $.SUBRULE($.optional) },
+        { ALT: () => $.SUBRULE($.repetition) }
+      ]);
+    });
+
+    $.RULE("optional", () => {
+      $.CONSUME(LSquare);
+      $.SUBRULE($.production);
+      $.CONSUME(RSquare);
+    });
+
+    $.RULE("repetition", () => {
+      $.CONSUME(LCurly);
+      $.SUBRULE($.production);
+      $.CONSUME(RCurly);
+    });
+  }
+}
+// Type:
+//   PrimitiveType
+// ReferenceType
+// PrimitiveType:
+//   {Annotation} NumericType
+//   {Annotation} boolean
+// NumericType:
+//   IntegralType
+//   FloatingPointType
+// IntegralType:
+//   (one of)
+// byte short int long char
+// FloatingPointType:
+//   (one of)
+// float double
+// ReferenceType:
+//   ClassOrInterfaceType
+//   TypeVariable
+//   ArrayType
--- src/parser_by_spec.js
+++ src/parser_by_spec.js
@@ -9,7 +9,146 @@ class JavaParser extends Parser {
 
     const $ = this;
 
-    $.RULE("compilationUnit", () => {});
+    // Productions from §7
+    // https://docs.oracle.com/javase/specs/jls/se11/html/jls-7.html
+    $.RULE("compilationUnit", () => {
+      $.OR([
+        { ALT: () => $.SUBRULE($.OrdinaryCompilationUnit) },
+        { ALT: () => $.SUBRULE($.ModularCompilationUnit) }
+      ]);
+    });
+
+    $.RULE("OrdinaryCompilationUnit", () => {
+      $.OPTION(() => {
+        $.SUBRULE($.PackageDeclaration);
+      });
+      $.MANY(() => {
+        $.SUBRULE($.ImportDeclaration);
+      });
+      $.MANY(() => {
+        $.SUBRULE($.TypeDeclaration);
+      });
+    });
+
+    $.RULE("ModularCompilationUnit", () => {
+      $.MANY(() => {
+        $.SUBRULE($.ImportDeclaration);
+      });
+      $.SUBRULE($.ModuleDeclaration);
+    });
+
+    $.RULE("PackageDeclaration", () => {
+      $.MANY(() => {
+        $.SUBRULE($.PackageModifier);
+      });
+
+      $.CONSUME(t.Package);
+      // Spec Deviation: extracted common pattern to QualifiedName rule.
+      $.SUBRULE($.QualifiedName);
+    });
+
+    $.RULE("PackageModifier", () => {
+      $.SUBRULE($.Annotation);
+    });
+
+    $.RULE("ImportDeclaration", () => {
+      $.OR([
+        { ALT: () => $.SUBRULE($.SingleTypeImportDeclaration) },
+        { ALT: () => $.SUBRULE($.TypeImportOnDemandDeclaration) },
+        { ALT: () => $.SUBRULE($.SingleStaticImportDeclaration) },
+        { ALT: () => $.SUBRULE($.StaticImportOnDemandDeclaration) }
+      ]);
+    });
+
+    // TODO refactor these import rules, they are not LL(K)
+    $.RULE("SingleTypeImportDeclaration", () => {
+      $.CONSUME(t.Import);
+      $.SUBRULE($.TypeName);
+      $.CONSUME(t.SemiColon);
+    });
+
+    $.RULE("TypeImportOnDemandDeclaration", () => {
+      $.CONSUME(t.Import);
+      $.SUBRULE($.PackageOrTypeName);
+      $.CONSUME(t.Dot);
+      $.CONSUME(t.Star);
+      $.CONSUME(t.SemiColon);
+    });
+
+    $.RULE("SingleStaticImportDeclaration", () => {
+      $.CONSUME(t.Import);
+      $.CONSUME(t.Static);
+      $.SUBRULE($.TypeName);
+      $.CONSUME(t.Dot);
+      $.SUBRULE($.Identifier);
+      $.CONSUME(t.SemiColon);
+    });
+
+    $.RULE("StaticImportOnDemandDeclaration", () => {
+      $.CONSUME(t.Import);
+      $.CONSUME(t.Static);
+      $.SUBRULE($.TypeName);
+      $.CONSUME(t.Dot);
+      $.CONSUME(t.Star);
+      $.CONSUME(t.SemiColon);
+    });
+
+    $.RULE("TypeDeclaration", () => {
+      $.OR([
+        { ALT: () => $.SUBRULE($.ClassDeclaration) },
+        { ALT: () => $.SUBRULE($.InterfaceDeclaration) },
+        { ALT: () => $.CONSUME(t.SemiColon) }
+      ]);
+    });
+
+    // {Annotation} [open] module Identifier {. Identifier} { {ModuleDirective} }
+    $.RULE("ModuleDeclaration", () => {
+      $.MANY(() => {
+        $.SUBRULE($.Annotation);
+      });
+      $.OPTION(() => {
+        $.CONSUME(t.Open);
+      });
+      $.CONSUME(t.Module);
+      // Spec Deviation: extracted common pattern to QualifiedName rule.
+      $.SUBRULE($.QualifiedName);
+      $.CONSUME(t.LCurly);
+      $.MANY(() => {
+        $.SUBRULE($.ModuleDirective);
+      });
+      $.CONSUME(t.RCurly);
+    });
+
+    $.RULE("ModuleDirective", () => {
+      // Spec Deviation: the 5 alternations of "ModuleDirective" were
+      //                 extracted to separate rules.
+      $.OR([
+        { ALT: () => $.SUBRULE($.RequiresModuleDirective) },
+        { ALT: () => $.SUBRULE($.ExportsModuleDirective) },
+        { ALT: () => $.SUBRULE($.OpensModuleDirective) },
+        { ALT: () => $.SUBRULE($.UsesModuleDirective) },
+        { ALT: () => $.SUBRULE($.ProvidesModuleDirective) }
+      ]);
+    });
+
+    $.RULE("RequiresModuleDirective", () => {});
+
+    $.RULE("ExportsModuleDirective", () => {});
+
+    $.RULE("OpensModuleDirective", () => {});
+
+    $.RULE("UsesModuleDirective", () => {});
+
+    $.RULE("ProvidesModuleDirective", () => {});
+
+    // Others
+    $.RULE("QualifiedName", () => {
+      $.SUBRULE($.Identifier);
+      $.MANY(() => {
+        $.CONSUME(t.Dot);
+        $.SUBRULE2($.Identifier);
+      });
+    });
   }
 }
 
